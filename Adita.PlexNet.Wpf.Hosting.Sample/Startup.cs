0a06d0c6aa2f61f59b2a487e76443bc4037a15fd
Setya Adi Kurnia <engineering.ins@weq-tech.com>
2022-10-26 13:53:39 +0700

    Add comment for InitializeComponent()

--- Adita.PlexNet.Wpf.Hosting.Sample/Startup.cs
+++ Adita.PlexNet.Wpf.Hosting.Sample/Startup.cs
@@ -20,6 +20,11 @@ namespace Adita.PlexNet.Wpf.Hosting.Sample
             });
 
             App app = builder.Build();
+
+            //if App has components such as StartupUri, subscribes event handler to event(s) or others in here need to call InitializeComponent()
+            //InitializeComponent method of App class only will generated after build if any component exist.
+            //Note: if you delete the StartupUri on App.xaml you need to handle Startup event, otherwise InitializeComponent will never be generated
+            //and your app resource will not be initialized too, this is such as bugs of wpf!!!.
             app.InitializeComponent();
             app.Run();
         }
